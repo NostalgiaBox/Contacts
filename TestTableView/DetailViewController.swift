4ddd118567856a8af2b1b58c9926a74dfa0df25a
NostalgiaBox <rfenster@gmail.com>
2019-11-26 20:29:46 -0500

    Initial Commit

---
+++ TestTableView/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  TestTableView
+//
+//  Created by Murray Fenstermaker on 11/26/19.
+//  Copyright © 2019 Nostalgiabox. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
